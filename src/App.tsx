d620152daef657400470b4f151dd0eaeb454b5b4
Alexander Dmitriev <alex.basrike@gmail.com>
2023-07-11 19:00:05 +0300

    finished to work with Lesson 07

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header />
+      <Technologies />
     </div>
   );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#!">Home</a>
+            <a href="#!">News Feed</a>
+            <a href="#!">Messages</a>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
