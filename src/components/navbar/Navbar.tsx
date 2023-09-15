3e7a5df9cf3dadeef7810bfa0977a5f9c11ce3a9
Alexander Dmitriev <alex.basrike@gmail.com>
2023-09-15 23:03:10 +0300

    worked with active page in primary navigation with the help of Route

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import css from './navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <aside className={css.aside}>
             <nav className={css.nav}>
                 <ul>
-                    <li className={css.active}><a href="/profile">Profile</a></li>
-                    <li><a href="/dialogs">Messages</a></li>
-                    <li><a href="/news">News</a></li>
-                    <li><a href="/music">Music</a></li>
-                    <li><a href="/settings">Settings</a></li>
+                    <li>
+                        <NavLink to="/profile" activeClassName={css.active}>Profile</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dialogs" activeClassName={css.active}>Messages</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/news" activeClassName={css.active}>News</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/music" activeClassName={css.active}>Music</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/settings" activeClassName={css.active}>Settings</NavLink>
+                    </li>
                 </ul>
             </nav>
         </aside>
