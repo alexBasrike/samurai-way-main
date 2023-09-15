2017d7e25119b7577066d60d8a02f9e972214684
Alexander Dmitriev <alex.basrike@gmail.com>
2023-09-15 23:24:29 +0300

    some demo content for dialogs

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,8 +3,23 @@ import css from './dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div className={css.module}>
-            Dialogs
+        <div className={css.dialogs}>
+
+            <div className={css.dialogsNames}>
+                <div className={css.item + ' ' + css.active}>Dimych</div>
+                <div className={css.item}>Andrey</div>
+                <div className={css.item}>Sveta</div>
+                <div className={css.item}>Sasha</div>
+                <div className={css.item}>Viktor</div>
+                <div className={css.item}>Valera</div>
+            </div>
+
+            <div className={css.dialogsMessages}>
+                <div className={css.item}>Hi</div>
+                <div className={css.item}>How is your it-kamasutra?</div>
+                <div className={css.item}>Yo</div>
+            </div>
+
         </div>
     )
 }
