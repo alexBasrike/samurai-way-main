cc3d1b86a81eb66d2b80ac3bd967b295310eb31b
Alexander Dmitriev <alex.basrike@gmail.com>
2023-10-20 21:21:01 +0300

    worked with method .map

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
 
-    const dialogsData = [
+    const dialogs = [
         {id: 1, name: "Dimych" },
         {id: 2, name: "Andrey" },
         {id: 3, name: "Sveta" },
@@ -13,7 +13,7 @@ export const Dialogs = () => {
         {id: 6, name: "Valera" }
     ]
 
-    const messagesData = [
+    const messages = [
         {id: 1, message: "Hi"},
         {id: 2, message: "How is your it-kamasutra?"},
         {id: 3, message: "Yo"},
@@ -21,17 +21,18 @@ export const Dialogs = () => {
         {id: 5, message: "Yo"}
     ]
 
+    const dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/> );
+    const messagesElements = messages.map( m => <Message message={m.message}/> );
+
     return (
         <div className={css.dialogs}>
 
             <div className={css.dialogsNames}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                { dialogsElements }
             </div>
 
             <div className={css.dialogsMessages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                { messagesElements }
             </div>
 
         </div>
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -4,10 +4,12 @@ import Post from "./post/Post";
 
 const MyPosts = () => {
 
-    const postData = [
+    const posts = [
         {id: 1, message: "Hi, how are you?", likesCount: 12},
         {id: 1, message: "It's my first post", likesCount: 11}
     ]
+
+    const postsElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount}/> );
 
     return (
         <div className={css.postsBlock}>
@@ -23,8 +25,7 @@ const MyPosts = () => {
             </div>
 
             <div className={css.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+                { postsElements }
             </div>
 
         </div>
